c2cce2f066c35c5d3b5d27bd14510fe55ef8de24
Thoams Freeborough <thomas@hotsnapper.com>
2016-12-06 13:56:10 +0000

    [Remove branding] Remove branding from site

--- src/app/route_components/home/Home.js
+++ src/app/route_components/home/Home.js
@@ -2,19 +2,12 @@ import React from 'react';
 
 import './Home.scss';
 
-import './logo.png';
-
 import ExampleContainer from '../../../_examples/example_container';
 
 const Home = (props) => {
   return (
     <div id="home">
       <h1>Application successfully running</h1>
-      <div id="logo-wrap">
-        <h1>Next generation applications brought to you by</h1>
-        <img src='/assets/logo.png' id="logo" />
-      </div>
-
       <ExampleContainer />
     </div>
   )
