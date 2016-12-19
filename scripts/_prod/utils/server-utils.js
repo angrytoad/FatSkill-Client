a5e1b43d8b26a9364a1fb6262077afc6affef19c
Thoams Freeborough <thomas@hotsnapper.com>
2016-12-19 22:55:52 +0000

    [Production] Small production changes

--- scripts/_prod/utils/server-utils.js
+++ scripts/_prod/utils/server-utils.js
@@ -5,9 +5,8 @@ export function createPage(html, initialState) {
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width,initial-scale=1">
-      <title>React App</title>
+      <title>Modernise Recruitment | Fatskill</title>
       <link rel="shortcut icon" href="/favicon.ico">
-      <link href="main.css" rel="stylesheet">
     </head>
     <body>
       <div id="root">${html}</div>
